e9cbfe201ddc266e2514dcf1b2953302a5762c7c
lacapsule05@gmail.com <lacapsule05@gmail.com>
2025-12-10 16:09:39 +0000

    changes string/int after DAG

--- models/00_staging_nettoyage_standardisation/01_transfo_historique_carriere.sql
+++ models/00_staging_nettoyage_standardisation/01_transfo_historique_carriere.sql
@@ -6,23 +6,23 @@ SELECT
     CAST(progression_salariale_pct AS FLOAT64) AS progression_salariale_pct, 
     CAST(nb_formations_suivies AS INT64) AS nb_formations_suivies,
     CASE
-        WHEN stagnation_detectee IN ('1') THEN TRUE
-        WHEN stagnation_detectee IN ('0') THEN FALSE
+        WHEN stagnation_detectee IN (1) THEN TRUE
+        WHEN stagnation_detectee IN (0) THEN FALSE
         ELSE NULL
     END AS stagnation_detectee,
     CASE
-        WHEN changement_manager_12mois IN ('1') THEN TRUE
-        WHEN changement_manager_12mois IN ('0') THEN FALSE
+        WHEN changement_manager_12mois IN (1) THEN TRUE
+        WHEN changement_manager_12mois IN (0) THEN FALSE
         ELSE NULL
     END AS changement_manager_12mois,
     CASE
-        WHEN demande_mutation_refusee IN ('1') THEN TRUE
-        WHEN demande_mutation_refusee IN ('0') THEN FALSE
+        WHEN demande_mutation_refusee IN (1) THEN TRUE
+        WHEN demande_mutation_refusee IN (0) THEN FALSE
         ELSE NULL
     END AS demande_mutation_refusee,
     CASE
-        WHEN potentiel_identifie IN ('1') THEN TRUE
-        WHEN potentiel_identifie IN ('0') THEN FALSE
+        WHEN potentiel_identifie IN (1) THEN TRUE
+        WHEN potentiel_identifie IN (0) THEN FALSE
         ELSE NULL
     END AS potentiel_identifie
 FROM {{ source('peoplepulse_data','historique_carriere') }}
--- models/00_staging_nettoyage_standardisation/02_transfo_absenteisme.sql
+++ models/00_staging_nettoyage_standardisation/02_transfo_absenteisme.sql
@@ -5,8 +5,8 @@ SELECT
     CAST(taux_absenteisme AS FLOAT64) AS taux_absenteisme,
     TRIM(motif_principal) AS motif_principal,
     CASE
-        WHEN absences_longue_duree IN ('1') THEN TRUE
-        WHEN absences_longue_duree IN ('0') THEN FALSE
+        WHEN absences_longue_duree IN (1) THEN TRUE
+        WHEN absences_longue_duree IN (0) THEN FALSE
         ELSE NULL
     END AS absences_longue_duree
 FROM {{ source('peoplepulse_data','absenteisme') }}
